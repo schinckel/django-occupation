dc24b1dfca16d8f0fe07dc1267f4a275c4ce466a
Matthew Schinckel <matt@schinckel.net>
2017-12-16 15:36:44 +1030

    Django Occupation is a Postgres multi-tenant package.
    It uses Row Level Security and triggers to ensure that data is protected.

---
+++ runtests.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+import warnings
+
+from django.core.management import execute_from_command_line
+
+warnings.simplefilter('error')
+
+try:
+    from psycopg2cffi import compat
+    compat.register()
+except ImportError:
+    pass
+
+
+def runtests():
+    argv = sys.argv[:1] + ['test', '--settings=tests.settings', '--noinput']
+    execute_from_command_line(argv)
+
+
+if __name__ == '__main__':
+    runtests()
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="occupation",
+    version='0.0.1',
+    description="Multitenancy support in django, via Row Level Security.",
+    url="https://bitbucket.org/schinckel/django-occupation",
+    author="Matthew Schinckel",
+    author_email="matt@schinckel.net",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'django',
+        'psycopg2',  # or psycopg2cffi under pypy
+    ],
+    classifiers=[
+        'Development Status :: 1 - Alpha',
+        'Framework :: Django :: 2.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    test_suite='runtests.runtests',
+)
