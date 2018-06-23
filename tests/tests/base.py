715cbf0f2c3c5b75ad9b0731a1328add50b2c212
Matthew Schinckel <matt@schinckel.net>
2018-06-24 08:45:26 +0930

    Missing files.

---
+++ tests/tests/base.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+from occupation.utils import get_tenant_model
+
+Tenant = get_tenant_model()
+
+
+class TenantTestCase(TestCase):
+    def build_tenants(self, count):
+        tenants = [
+            Tenant(name='{}'.format(i))
+            for i in range(count)
+        ]
+        Tenant.objects.bulk_create(tenants)
+        return tenants
---
+++ tests/tests/test_admin.py
@@ -0,0 +1,31 @@
+import unittest
+
+from django.contrib.auth.models import User, Permission
+
+from .base import TenantTestCase
+from ..models import RestrictedModel
+
+
+class TestAdmin(TenantTestCase):
+    def user(self):
+        user = User.objects.create(
+            username='username',
+            password='password',
+            is_staff=True,
+        )
+        user.user_permissions.add(*Permission.objects.all())
+        return user
+
+    @unittest.expectedFailure
+    def test_related_objects_only_show_from_active_tenant(self):
+        a, b = self.build_tenants(2)
+        RestrictedModel.objects.bulk_create([
+            RestrictedModel(tenant=a, name='a'),
+            RestrictedModel(tenant=b, name='b'),
+            RestrictedModel(tenant=a, name='c'),
+        ])
+        user = self.user()
+        self.client.force_login(user)
+
+        response = self.client.get('/admin/tests/restrictedmodel/')
+        self.assertEqual(0, response.context['cl'].queryset.count())
