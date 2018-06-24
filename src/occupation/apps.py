9fef2fa3018c984fd2d36fabad2c26715fd4cd94
Matthew Schinckel <matt@schinckel.net>
2018-06-24 16:52:16 +0930

    Add checks and tests for checks.

--- src/occupation/apps.py
+++ src/occupation/apps.py
@@ -2,6 +2,14 @@ from django.apps import AppConfig
 from django.core.checks import Error, Warning, register
 
 
+MIDDLEWARE = [
+    'occupation.middleware.SelectTenant',
+    'occupation.middleware.ActivateTenant',
+]
+
+CONTEXT = 'occupation.context_processors.tenants'
+
+
 class OccupationConfig(AppConfig):
     name = 'occupation'
 
@@ -26,24 +34,101 @@ class OccupationConfig(AppConfig):
         patch_admin()
 
 
+def session_middleware_index(settings):
+    for i, middleware in enumerate(settings.MIDDLEWARE):
+        if middleware.endswith('.SessionMiddleware'):
+            return i
+
+
+@register('settings')
+def check_session_middleware_installed(app_configs=None, **kwargs):
+    from django.conf import settings
+
+    if session_middleware_index(settings) is None:
+        return [Error(
+            'It appears that no session middleware is installed.',
+            hint="Add 'django.contrib.sessions.middleware.SessionMiddleware' to your MIDDLEWARE",
+            id='occupation.E001',
+        )]
+    return []
+
+
 @register('settings')
 def check_middleware_installed_correctly(app_configs=None, **kwargs):
-    # Installed after SessionMiddleware.
-    Error
-    return []
+    from django.conf import settings
+
+    errors = []
+
+    session = session_middleware_index(settings)
+
+    if session is not None:
+        for middleware in MIDDLEWARE:
+            if middleware not in settings.MIDDLEWARE:
+                errors.append(
+                    Error(
+                        'The middleware "{}" is not installed.'.format(middleware),
+                        id='occupation.E002',
+                    )
+                )
+            elif settings.MIDDLEWARE.index(middleware) < session:
+                errors.append(
+                    Error(
+                        'The occupation middleware "{}" is installed before the session middleware.'.format(
+                            middleware
+                        ),
+                        id='occupation.E003',
+                    )
+                )
+
+    try:
+        if settings.MIDDLEWARE.index(MIDDLEWARE[1]) < settings.MIDDLEWARE.index(MIDDLEWARE[0]):
+            errors.append(
+                Error(
+                    'The middleware "{1}" must be installed after "{0}".'.format(*MIDDLEWARE),
+                    id='occupation.E004'
+                )
+            )
+    except ValueError:
+        pass
+
+    return errors
 
 
 @register('settings')
-def check_context_manager_installed(app_configs=None, **kwargs):
+def check_context_processor_installed(app_configs=None, **kwargs):
     # Warning if not.
-    Warning
-    return []
+    from django.conf import settings
+
+    errors = []
+
+    for i, engine in enumerate(settings.TEMPLATES):
+        if engine['BACKEND'] != 'django.template.backends.django.DjangoTemplates':
+            continue
+        if CONTEXT not in engine.get('OPTIONS', {}).get('context_processors', []):
+            errors.append(Warning(
+                'Missing context processor "{}"'.format(CONTEXT),
+                hint="Add '{1}' to settings.TEMPLATES[{0}]['OPTIONS']['context_processors']".format(
+                    i, CONTEXT
+                ),
+                id='occupation.W001',
+            ))
+
+    return errors
 
 
 @register('settings')
 def check_installed_before_admin(app_configs=None, **kwargs):
+    from django.conf import settings
+
     # Warning if not, as we can't override templates.
-    Warning
+    if 'django.contrib.admin' in settings.INSTALLED_APPS:
+        if settings.INSTALLED_APPS.index('django.contrib.admin') < settings.INSTALLED_APPS.index('occupation'):
+            return [
+                Warning(
+                    "'occupation' must be installed prior to 'django.contrib.admin', to allow overridding templates.",
+                    id='occupation.W002'
+                )
+            ]
     return []
 
 
---
+++ tests/tests/test_settings.py
@@ -0,0 +1,64 @@
+from copy import deepcopy
+
+from django.conf import settings
+from django.test import TestCase, modify_settings
+
+from occupation import apps
+
+
+class TestSettings(TestCase):
+    def test_all_settings(self):
+        self.assertEqual([], apps.check_session_middleware_installed())
+        self.assertEqual([], apps.check_middleware_installed_correctly())
+        self.assertEqual([], apps.check_context_processor_installed())
+        self.assertEqual([], apps.check_installed_before_admin())
+
+    @modify_settings(MIDDLEWARE={'remove': apps.MIDDLEWARE})
+    def test_middleware_missing(self):
+        errors = apps.check_middleware_installed_correctly()
+        self.assertEqual(2, len(errors))
+        self.assertEqual('occupation.E002', errors[0].id)
+        self.assertEqual('occupation.E002', errors[1].id)
+
+    @modify_settings(MIDDLEWARE={'remove': ['django.contrib.sessions.middleware.SessionMiddleware']})
+    def test_session_middleware_missing(self):
+        errors = apps.check_session_middleware_installed()
+        self.assertEqual(1, len(errors))
+        self.assertEqual('occupation.E001', errors[0].id)
+
+    @modify_settings(MIDDLEWARE={
+        'remove': apps.MIDDLEWARE,
+        'append': apps.MIDDLEWARE[::-1],
+    })
+    def test_middleware_order_incorrect(self):
+        errors = apps.check_middleware_installed_correctly()
+        self.assertEqual(1, len(errors))
+        self.assertEqual('occupation.E004', errors[0].id)
+
+    @modify_settings(MIDDLEWARE={
+        'remove': apps.MIDDLEWARE,
+        'prepend': apps.MIDDLEWARE,
+    })
+    def test_middleware_before_session_middleware(self):
+        errors = apps.check_middleware_installed_correctly()
+        self.assertEqual(2, len(errors))
+        self.assertEqual('occupation.E003', errors[0].id)
+        self.assertEqual('occupation.E003', errors[1].id)
+
+    @modify_settings(INSTALLED_APPS={
+        'remove': 'occupation',
+        'append': 'occupation',
+    })
+    def test_installed_before_admin(self):
+        errors = apps.check_installed_before_admin()
+        self.assertEqual(1, len(errors))
+        self.assertEqual('occupation.W002', errors[0].id)
+
+    @modify_settings()
+    def test_missing_context_manager(self):
+        templates = deepcopy(settings.TEMPLATES)
+        templates[0]['OPTIONS']['context_processors'].remove(apps.CONTEXT)
+        with self.settings(TEMPLATES=templates):
+            errors = apps.check_context_processor_installed()
+            self.assertEqual(1, len(errors))
+            self.assertEqual('occupation.W001', errors[0].id)
